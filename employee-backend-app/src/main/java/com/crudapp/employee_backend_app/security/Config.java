e23b34ce070129f3cf46ebba0f7a1cd56e56853b
emreynr <emre.yenrr@gmail.com>
2024-05-24 14:09:07 +0300

    Defined user based roles

--- employee-backend-app/src/main/java/com/crudapp/employee_backend_app/security/Config.java
+++ employee-backend-app/src/main/java/com/crudapp/employee_backend_app/security/Config.java
@@ -4,9 +4,13 @@ package com.crudapp.employee_backend_app.security;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Config {
@@ -33,5 +37,31 @@ public class Config {
                 .build();
 
         return new InMemoryUserDetailsManager(sarah, jack, michael);
+    }
+
+    @Bean
+
+    // role based restrict
+    /*
+    GET   -> /api/employees/             -> READ ALL -> "EMPLOYEE"
+    GET   -> /api/employees/{employeeId} -> READ ONE -> "EMPLOYEE"
+    POST  -> /api/employees/             -> SAVE     -> "MANAGER"
+    PUT   -> /api/employees/             -> UPDATE   -> "MANAGER"
+    DELETE ->/api/employees/{employeeId} -> DELETE   -> "ADMIN"
+     */
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
